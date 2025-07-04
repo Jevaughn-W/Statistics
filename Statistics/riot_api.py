186766ff86230de904f73996d79b5a56c8f959d4
Jevaughn-W <jevaughn.williamsx@gmail.com>
2025-07-03 21:33:32 -0400

    Add request to pull match data for the list of matches obtained

--- Statistics/riot_api.py
+++ Statistics/riot_api.py
@@ -1,6 +1,6 @@
 import requests #Package for making API calls
 import sys #Package allowing for the use of variables in CLI
-import json #allows pythong to convert json response to an object
+import json #allows python to convert json response to an object
 
 #Get riot account PUUID using Account -v1 API point: get account by riot ID
 gameName = "xLazor"
@@ -14,12 +14,21 @@ accountResponse = requests.get(f"{api}{getAccountEndPoint}") #API call
 
 puuid = (accountResponse.json()['puuid']) #string of puuid which is needed to call match IDs
 
-#print(puuid)
 
 #Get riot matchIDs using Match-v5 API point: get account by PUUID
 
-getMatchesEndPoint = f"/lol/match/v5/matches/by-puuid/{puuid}/ids?/start=0&count=100&api_key={apiKey}" #To update and pass in variables in better manner
+getMatchesEndPoint = f"/lol/match/v5/matches/by-puuid/{puuid}/ids?/start=0&count=20&api_key={apiKey}" #To update and pass in variables in better manner
 
 matchResponse = requests.get(f"{api}{getMatchesEndPoint}") #API call
 
-print(matchResponse.json())
+matchArr = matchResponse.json() #saves match IDs as an array
+
+#Get match information using Match-v5 API endpoint: get a matchline by match id
+matchData = []
+
+for match in matchArr:
+    GetMatchTimelineEndPoint= f"/lol/match/v5/matches/{match}/timeline?api_key={apiKey}"
+    matchTimeline = requests.get(f"{api}{GetMatchTimelineEndPoint}") #API call for Match data
+    matchData.append(matchTimeline) #add 200 response for each match
+
+print(matchData)
