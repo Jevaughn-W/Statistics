5bdb0b5d59dc2d8076f1cf7106bfe5ae66433baa
Jevaughn-W <jevaughn.williamsx@gmail.com>
2025-07-07 22:34:33 -0400

    Converted data to CSV file and added count of matches to pass as an argument

--- Statistics/riot_api.py
+++ Statistics/riot_api.py
@@ -1,11 +1,13 @@
 import requests #Package for making API calls
 import sys #Package allowing for the use of variables in CLI
 import json #allows python to convert json response to an object
+import pandas as pd #allows transformation of data and writing to CSV file
 
 #Get riot account PUUID using Account -v1 API point: get account by riot ID
 gameName = "xLazor"
 tagLine = "NA1"
 apiKey = sys.argv[1] #Enter Riot key when running script
+countOfMatchesRequested = sys.argv[2] #Error when using 100 matches -- Check if there is a limit of requests per seconds
 api = 'https://americas.api.riotgames.com'
 getAccountEndPoint = f"/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}?api_key={apiKey}" #API with variables note thatf is added to interpolate
 
@@ -17,7 +19,7 @@ puuid = (accountResponse.json()['puuid']) #string of puuid which is needed to ca
 
 #Get riot matchIDs using Match-v5 API point: get account by PUUID
 
-getMatchesEndPoint = f"/lol/match/v5/matches/by-puuid/{puuid}/ids?/start=0&count=20&api_key={apiKey}" #To update and pass in variables in better manner
+getMatchesEndPoint = f"/lol/match/v5/matches/by-puuid/{puuid}/ids?/start=0&count={countOfMatchesRequested}&api_key={apiKey}" #To update and pass in variables in better manner
 
 matchResponse = requests.get(f"{api}{getMatchesEndPoint}") #API call
 
@@ -35,4 +37,10 @@ for match in matchArr:
         "startTime": matchTimeline.json()["info"]["gameCreation"],
         }) #add response (start, end and match ID) for each match
 
-print(matchData)
+# Creating structuring data and write to CSV file
+
+df = pd.DataFrame(
+    matchData
+)
+
+df.to_csv('match_history.csv', index=True)
