7cad679e9b1b38de617e5fdd97212592b428d3a0
Jevaughn-W <jevaughn.williamsx@gmail.com>
2025-07-10 19:15:54 -0400

    Add main file and dates column to dataframe

--- Statistics/main.py
+++ Statistics/main.py
@@ -3,15 +3,20 @@ import convert_timestamp as convert_timeStamp
 
 df = pd.read_csv('.\match_history.csv') #Read in match history as data frame
 
-#print(df)
+convertedColumn = [] #Placeholder for updated Date
 
-for time in df["startTime"]:
-        print(convert_timeStamp.convertTimeStamp(time))
+#Converts startTime timestap to date
+for timeStamp in df['startTime']:
+        convertedColumn.append(convert_timeStamp.convertTimeStamp(timeStamp))
+
+df['gameDates'] = convertedColumn #add new column
+
+print(df)
 
 
 '''
 Next Steps:
-1. Update dataframe with updated column
+1. Update dataframe with updated column - Complete
 2. Write function to summarize by day and count of games
 3. Estimate the average games per day
 4. Check out what is causing cap on number of records written
