7c3218bd534dfef93793212333b797d246aba895
Jevaughn-W <jevaughn.williamsx@gmail.com>
2025-07-10 21:18:45 -0400

    Added in modules to calculate Possion PMF and plot results

--- Statistics/main.py
+++ Statistics/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import convert_timestamp as convert_timeStamp
 import count_daily_game as count_daily_game
+import matplotlib.pyplot as plt 
+from scipy.stats import poisson
+import numpy as np
 
 df = pd.read_csv('.\match_history.csv') #Read in match history as data frame
 
@@ -22,15 +25,35 @@ def findAverageDailyGames(gameDict): #Calculates average of the daily games play
                 totalGames = gameDict[day] + totalGames
         return totalGames / countofRecords
 
-print(findAverageDailyGames(gamesPlayedPerDay))
+avgGamesPerDay = findAverageDailyGames(gamesPlayedPerDay)
 
+# Manual calculation of PMF
+# def poissonPlots(avgGames): #Function to calculate the plot points for a poisson distribution
+#         result = {}
+#         for i in range(5):
+#                 result[i] = (math.exp(-avgGames) * ((avgGames) ** i)/ math.factorial(i))
+#         return result
+
+xAxis = np.arange(10) #Creating the x axis
+pmf = poisson.pmf(xAxis, avgGamesPerDay) #Calculating PMF for each k
+
+#Details for plotting distribution
+
+plt.figure(figsize=(8, 5))
+plt.bar(xAxis, pmf, color='skyblue', edgecolor='black')
+plt.title(f'Poisson Distribution (λ = {avgGamesPerDay})')
+plt.xlabel('Number of Events (k)')
+plt.ylabel('Probability')
+plt.xticks(xAxis) # Ensure integer ticks on the x-axis
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
 
 
 
 '''
 Next Steps:
 1. Update dataframe with updated column - Complete
-2. Write function to summarize by day and count of games
-3. Estimate the average games per day
+2. Write function to summarize by day and count of games - Complete
+3. Estimate the average games per day - Complete
 4. Check out what is causing cap on number of records written
 '''
